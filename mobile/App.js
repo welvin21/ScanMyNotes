342bf3535cb4ab8ec12dfee9a11c4679d3cde4cc
Welvin <wewe2712@connect.hku.hk>
2019-11-19 00:28:18 +0700

    camera : Add camera screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CameraScreen from './screens/camera/CameraScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <CameraScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
