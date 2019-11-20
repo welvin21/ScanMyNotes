9c1190634e81932490f90561d6d6fe53accf23c8
welvin21 <47080921+welvin21@users.noreply.github.com>
2019-11-20 13:39:39 +0700

    typescript : migrate to typescript (#2)
    * camera: Enable access to system's gallery
    
    * typescript : migrate to typescript

--- mobile/screens/camera/CameraScreen.tsx
+++ mobile/screens/camera/CameraScreen.tsx
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createRef, RefObject } from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import * as Gallery from 'expo-image-picker';
 import { Camera } from 'expo-camera';
 
-import { MaterialCommunityIcons,Ionicons } from '@expo/vector-icons';
+import { Ionicons,FontAwesome } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     backgroundColor: 'transparent',
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end'
   },
-  flipper : {
-    position: 'absolute',
-    left: 0,
-    bottom: 0
-  },
-  capture: {
-    position: 'absolute',
-    left: '50%',
-    bottom: 0,
-    marginLeft: -40,
+  sider: {
+    marginBottom: 20,
   }
 })
 
 const CameraScreen = () => {
     const [ hasCameraPermission, changeCameraPermission ] = useState(null);
     const [ cameraType, changeCameraType ] = useState(Camera.Constants.Type.back);
-    const cameraRef = React.createRef();
+    const cameraRef = createRef<Camera>();
 
     useEffect(() => {
         const askForPermission = async () => {
-          const { status } = await Permissions.askAsync(Permissions.CAMERA);
+          const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
           changeCameraPermission(status === 'granted');
         }
         askForPermission();
@@ -44,23 +40,28 @@ const CameraScreen = () => {
             <Camera style={{ flex: 1 }} type={ cameraType } ref={ cameraRef }>
                 <View style={ styles.container }>
                   <TouchableOpacity
-                      style={ styles.flipper }
+                      style={ styles.sider }
                       onPress={() => { changeCameraType(
                           cameraType === Camera.Constants.Type.back
                           ? Camera.Constants.Type.front
                           : Camera.Constants.Type.back
-                      ) }}
-                  >
-                      <MaterialCommunityIcons name='camera-party-mode' size={32} color='#fff'/>
+                      ) }}>
+                      <Ionicons name='md-reverse-camera' size={32} color='#fff'/>
                   </TouchableOpacity>
                   <TouchableOpacity
-                      style={ styles.capture }
                       onPress={async() => {
-                        let photo = await cameraRef.current.takePictureAsync();
-                        console.log(photo);
+                        const photo = await cameraRef.current.takePictureAsync();
+                        console.log(photo.uri);
                       }}
                   >
                       <Ionicons name='ios-radio-button-on' size={80} color='#fff'/>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={ styles.sider }
+                      onPress={ ()=> {
+                        Gallery.launchImageLibraryAsync({mediaTypes: Gallery.MediaTypeOptions.Images, allowsEditing: true});
+                      } }>
+                      <FontAwesome name='picture-o' size={32} color='#fff'/>
                   </TouchableOpacity>
                 </View>
             </Camera>
