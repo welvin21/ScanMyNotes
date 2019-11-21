c081396e76ee80086bbbf3fb34c512054eb8ccef
welvin21 <47080921+welvin21@users.noreply.github.com>
2019-11-21 16:39:29 +0700

    result : merge branch result for result screen (#4)
    * result : Modify result screen and enable api key for OCR
    
    * result : add loader component used while fetching data from API call
    
    * result : change result data to avoid error

--- mobile/screens/camera/ResultScreen.tsx
+++ mobile/screens/camera/ResultScreen.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import env from '../../env';
+import Loader from '../../components/Loader';
+
+const axios = require('axios');
 
 const Result = props => {
-  return(
-    <View style={{flex : 1}} >
-      <Text style={{flex : 1}}>placeholder</Text>
-    </View>
-  )
+  const url = props.navigation.getParam('uri',null);
+  const [ result, changeResult ] = useState(null);
+
+  useEffect(() => {
+    const getResult = async() => {
+      let formData = new FormData();
+      formData.append('apikey',env.apikey);
+      formData.append('url',url);
+      
+      axios.post('https://api.ocr.space/parse/image',formData,{
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then( res => {
+        const { ParsedResults } = res.data;
+        changeResult('output');
+      } )
+      .catch( err => console.error(err) );
+    };
+
+    setInterval( () => {
+      getResult();
+    }, 3500);
+
+  },[]);
+
+  if(!result){
+    return(
+      <View style={{flex : 1}} >
+        <Loader/>
+      </View>
+    );
+  }else{
+    return(
+      <View style={{flex : 1}} >
+        <Text style={{top : '40%', position: 'absolute'}}>{result}</Text>
+      </View>
+    );
+  }
 }
 
 export default Result;
