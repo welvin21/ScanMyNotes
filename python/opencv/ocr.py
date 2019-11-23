c636d535377d248c518e71a63e8e101fc3a08f08
welvin21 <47080921+welvin21@users.noreply.github.com>
2019-11-24 01:38:54 +0700

    ocr : improve ocr accuracy by pre-processing techniques (#7)
    * ocr : add grayscaling and binarization before ocr
    
    * ocr : add interpolation and fix bugs
    
    * ocr : image rescaling and output text alignment

--- python/opencv/ocr.py
+++ python/opencv/ocr.py
@@ -2,13 +2,29 @@ import pytesseract
 import cv2
 
 class OCR:
-  def getText(img,lang):
-    # enlarge image
-    scale_percent = 100
-    w = int(img.shape[1] * scale_percent / 100)
-    h = int(img.shape[0] * scale_percent / 100)
-    img = cv2.resize(img,(w,h))
+  def preProcess(img):
+    # image interpolation using bicubic method
+    img = cv2.resize(img,None, fx=1, fy=1, interpolation=cv2.INTER_CUBIC)
 
-    # add white border to image
-    img = cv2.copyMakeBorder(img,100,100,100,100,cv2.BORDER_CONSTANT,(255,255,255))
-    return pytesseract.image_to_string(img,lang=lang)
+    # convert image to graycale format
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # pixel value binarization ( 0 or 255 )
+    h,w = img.shape[:2]
+    for i in range(h):
+      for j in range(w):
+        if(img[i,j] <= 160):
+          img[i,j] = 0
+        else:
+          img[i,j] = 255
+
+    #add white border
+    img = cv2.copyMakeBorder(img,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
+    return img
+  def getText(img):
+    config = ('-l eng --oem 1 --psm 8')
+    txt = pytesseract.image_to_string(img,config=config)
+    # if(txt == ''):
+    #   cv2.imshow('image',img)
+    #   cv2.waitKey(0)
+    return txt
